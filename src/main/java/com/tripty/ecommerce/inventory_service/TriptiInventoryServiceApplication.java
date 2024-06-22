9f0b43496751c54c9996c1c78c40e6408a0b5f3f
abhishek <kr.skabhi@gmail.com>
2024-06-23 05:12:15 +0530

    first commit

---
+++ src/main/java/com/tripty/ecommerce/inventory_service/TriptiInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tripty.ecommerce.inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptiInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptiInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripty/ecommerce/inventory_service/TriptiInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripty.ecommerce.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptiInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
